12c7214e397ec0ec6fd4b7424eb50959a611c7ae
Mendibj <37113509+Mendibj@users.noreply.github.com>
2018-06-18 21:32:05 +0200

    Things

--- src/sudoku/Main.java
+++ src/sudoku/Main.java
@@ -1,12 +1,13 @@
 package sudoku;
 
-import java.util.List;
-
 public class Main {
 
 	public static void main(String[] args) {
+		
+		//EJEMPLO DE SUDOKU RESOLUBLE
 		Sudoku s = new Sudoku("ENTRADA.txt");
-		System.out.println(s.solve());
+		if(s.solve()) System.out.println("Éxito al resolver");
+		else System.out.println("No se pudo resolver");
 		System.out.println(s);
 
 	}
